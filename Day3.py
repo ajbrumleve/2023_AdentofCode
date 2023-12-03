87fd6d79a8475287bb4f9a4bcfc2b681127bc40a
ajbrumleve <ajbrumleve@gmail.com>
2023-12-02 21:46:49 -0600

    Added day 3

---
+++ Day3.py
@@ -0,0 +1,22 @@
+def part1():
+    val = 0
+    with open('test_input_1', 'r') as f:
+        for line in f:
+
+    return val
+
+
+
+
+
+def part2():
+    val = 0
+    with (open('test_input_2', 'r') as f):
+        for line in f:
+
+
+    return val
+
+
+answer1 = part1()
+# answer2 = part2()
